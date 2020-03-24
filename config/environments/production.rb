5e191971ad01a953308918b2988b5d688ba59691
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-03-24 23:13:39 +0100

    extend active storage urls validity (for long uploads)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,8 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  config.active_storage.service_urls_expire_in = 1.hour
+
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
