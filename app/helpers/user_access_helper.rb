2f7f1e62634de021866f6f1586a4c0a4fdaa029d
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-03-25 02:08:19 +0100

    add complicated date calculation helper

--- app/helpers/user_access_helper.rb
+++ app/helpers/user_access_helper.rb
@@ -10,5 +10,22 @@ module UserAccessHelper
   def user_has_limited_access?
     user_signed_in? && current_user.in_trial_period?
   end
+
+  def days_till_end_of_subscription user
+    return nil if user.admin?
+
+    current_subscription = user.subscriptions.where("date_start <= ? AND date_end >= ?", DateTime.current, DateTime.current).first
+
+    return nil if current_subscription.nil?
+
+    (current_subscription.date_end.to_date - Date.current).to_i
+  end
+
+  def days_till_end_of_trial user
+    return nil if !user.in_trial_period?
+    return nil if user.admin?
+
+    7 - (Date.current - user.confirmed_at.to_date).to_i
+  end
 end
 
---
+++ test/helpers/user_access_helper_test.rb
@@ -0,0 +1,27 @@
+# SPDX-FileCopyrightText: 2020 Felix Wolfsteller
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+require 'test_helper'
+
+class UserAccessHelperTest < ActionView::TestCase
+  test "#days_till_end_of_subscription" do
+    sue = User.create(email: 'sue@s.an', password: 'password',
+                      password_confirmation: 'password', confirmed_at: DateTime.now)
+    assert_nil days_till_end_of_subscription(sue)
+
+    sue.subscriptions.create(date_start: DateTime.now - 1.hours, date_end: DateTime.now + 4.days)
+
+    assert_equal 4, days_till_end_of_subscription(sue)
+
+    assert_nil days_till_end_of_subscription(users(:admin))
+  end
+
+  test "#days_till_end_of_trial" do
+    sue = User.create(email: 'sue@s.an', password: 'password',
+                      password_confirmation: 'password', confirmed_at: DateTime.now - 1.days)
+    assert_equal 6, days_till_end_of_trial(sue)
+
+    assert_nil days_till_end_of_trial(users(:admin))
+  end
+end
