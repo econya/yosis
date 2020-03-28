5352358ee1b50e276e07748c2323e676899597b9
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-03-28 19:35:52 +0100

    set default sender mail, add spdx header

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,8 @@
+# SPDX-FileCopyrightText: 2020 Felix Wolfsteller
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: ENV['SENDER_EMAIL'] || 'yosismail'
   layout 'mailer'
 end
