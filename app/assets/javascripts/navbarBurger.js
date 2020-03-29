873a27853e2758dc86e65d0a07cb301d662f98d1
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-03-29 20:14:49 +0200

    add delete button js handler (notifications), turbolinks

--- app/assets/javascripts/navbarBurger.js
+++ app/assets/javascripts/navbarBurger.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('turbolinks:load', () => {
   // Get all "navbar-burger" elements
   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
