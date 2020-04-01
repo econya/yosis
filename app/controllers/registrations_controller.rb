2d9d8f32078891e9205197abe7a327f4b9ef3207
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-04-01 11:47:51 +0200

    add registration_controller override

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2020 Felix Wolfsteller
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+class RegistrationsController < Devise::RegistrationsController
+  def create
+    super
+    if @user.persisted?
+      AdminMailer.new_registration(@user).deliver #later
+    end
+  end
+end
