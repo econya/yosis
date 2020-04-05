c5e37356d87345e06c30f30c285d4afea43c0b34
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-04-05 16:02:30 +0200

    add User scopes: with_current_subscription, ...
    with_no_current_subscription, trial_started_at, in_trial

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,19 +11,38 @@ class User < ApplicationRecord
 
   has_many :subscriptions
 
+  scope :with_current_subscription, -> {
+    joins(:subscriptions).merge(Subscription.current)
+  }
+  scope :with_no_current_subscription, -> {
+    # OPTIMIZE so that we do not use subquery
+    where.not(id: with_current_subscription)
+  }
+  scope :in_trial, -> {
+    # OPTIMIZE so that we do not use subqueries
+    where.not("confirmed_at < ?", DateTime.current - 7.days)
+    .where(id: with_no_current_subscription)
+  }
+  scope :trial_started_at, ->(day: Date.current - 5.days) {
+    where(confirmed_at: day.all_day)
+  }
+
   def admin?
     role == 'admin'
   end
 
   def currently_subscribed?
+    # TODO: use scope
     subscribed_at?(DateTime.current)
   end
 
   def subscribed_at? datetime
+    # TODO: use scope
     admin? || subscriptions.where("date_start <= ? AND date_end >= ?", datetime, datetime).present?
   end
 
   def in_trial_period?
+    # TODO: use scope
     return false if admin?
     return false if currently_subscribed?
     return false if confirmed_at.nil?
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -23,4 +23,45 @@ class UserTest < ActiveSupport::TestCase
     userx.update(confirmed_at: DateTime.current - 8.day)
     assert_not userx.in_trial_period?
   end
+
+  test 'scope #in_trial' do
+    # fixture data has none in trial
+    assert_equal [], User.in_trial
+
+    userx = users(:user)
+    userx.update(confirmed_at: DateTime.current - 1.day)
+    assert_equal [userx], User.in_trial.to_a
+
+    userx.update(confirmed_at: DateTime.current - 8.days)
+
+    assert_equal [], User.in_trial.to_a
+
+    userx.subscriptions.create(date_start: DateTime.current - 8.days, date_end: DateTime.current + 8.days)
+    assert_equal [], User.in_trial.to_a
+
+    # Now user is subscribed, but in trial wrt confirmation date
+    userx.update(confirmed_at: DateTime.current - 2.days)
+    assert_equal [], User.in_trial.to_a
+  end
+
+  test 'scope #with_current_subscription' do
+    # fixture data has none in trial
+    assert_equal [], User.with_current_subscription
+
+    userx = users(:user)
+    userx.subscriptions.create(date_start: DateTime.current - 8.days, date_end: DateTime.current + 8.days)
+    assert_equal [userx], User.with_current_subscription
+  end
+
+  test 'scope #trial_started_at' do
+    # fixture data has none in trial
+    assert_equal [], User.trial_started_at
+
+    userx = users(:user)
+    userx.update(confirmed_at: DateTime.current - 7.days)
+    assert_equal [userx], User.trial_started_at(day: DateTime.current - 7.days)
+
+    userx.update(confirmed_at: DateTime.current - 6.days)
+    assert_equal [], User.trial_started_at(day: DateTime.current - 7.days)
+  end
 end
