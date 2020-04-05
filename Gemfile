7a4b35b7a0dda2d4d0a9f18d0f07ac255288fb99
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-04-05 16:04:39 +0200

    Add SPDX reuse compliance headers

--- Gemfile
+++ Gemfile
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2020 Felix Wolfsteller
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2020 Felix Wolfsteller
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 class ApplicationJob < ActiveJob::Base
   # Automatically retry jobs that encountered a deadlock
   # retry_on ActiveRecord::Deadlocked
