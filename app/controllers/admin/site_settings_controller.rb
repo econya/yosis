605f5b8d1555775b2f57377797ef2fed47ba74c4
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-04-06 12:19:04 +0200

    Add payment_details setting.

--- app/controllers/admin/site_settings_controller.rb
+++ app/controllers/admin/site_settings_controller.rb
@@ -12,7 +12,7 @@ class Admin::SiteSettingsController < ApplicationController
   def index
     md_keys = [:intro, :explanation, :privacy_statement, :terms, :impressum,
                :copyright_notice, :about_us, :trial_period_cta,
-               :register_cta]
+               :register_cta, :payment_details]
     md_keys.each do |key|
       SiteSetting.find_or_create_by(key: key,
         kind: "markdown",
@@ -31,7 +31,7 @@ class Admin::SiteSettingsController < ApplicationController
     SiteSetting.find_or_create_by(key: 'your_name',
       value: t('site_settings.your_name.default'), kind: 'string')
 
-    @general_settings    = SiteSetting.where(key: ['copyright_notice', 'title', 'logo', 'your_name'])
+    @general_settings    = SiteSetting.where(key: ['copyright_notice', 'title', 'logo', 'your_name', 'payment_details'])
     @start_page_settings = SiteSetting.where(key: ['intro', 'intro_background', 'register_cta', 'trial_period_cta'])
     @pages_settings      = SiteSetting.where(key: ['about_us', 'impressum', 'privacy_statement', 'terms', 'explanation'])
   end
