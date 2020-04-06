130b81116207d571fba03238a172d2c7740ee580
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-04-06 12:28:47 +0200

    Add the contact mailer, too.

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2020 Felix Wolfsteller
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+class ContactMailer < ApplicationMailer
+  default to: ENV['SENDER_EMAIL'] || 'yosis'
+
+  def feedback()
+    @user    = params[:user]
+    @contact = Contact.new(subject: params[:subject],
+                           message: [params[:messag]])
+
+    mail(subject: t('contact_mailer.contacted'), reply_to: @user.email)
+  end
+end
