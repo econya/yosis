28823074ef3181dc3f90316d0e5d57d182c9f8e1
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-09-21 09:21:39 +0200

    add ignorecase helo in ApplicationSystemTestCase

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,13 @@
+# SPDX-FileCopyrightText: 2020 Felix Wolfsteller
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+
+  def ignorecase str
+    Regexp.new(str, Regexp::IGNORECASE)
+  end
 end
