dfb77e364a96cdb8459bc911e5319d8485c3b539
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-10-05 19:14:17 +0200

    mess with migration to reflect new validations

--- db/migrate/20200325204229_add_row_order_to_lessons.rb
+++ db/migrate/20200325204229_add_row_order_to_lessons.rb
@@ -8,7 +8,9 @@ class AddRowOrderToLessons < ActiveRecord::Migration[6.0]
 
     # Newest Lesson will rank highest (be first).
     Lesson.all.order(created_at: :desc).each do |lesson|
-      lesson.update!(row_order: lesson.created_at.to_i)
+      # lesson.update!(row_order: lesson.created_at.to_i)
+      # For compatibility with "newer" validations and rank_order definition
+      Lesson.connection.execute("UPDATE lessons SET row_order = #{lesson.created_at.to_i} WHERE id = #{lesson.id}")
     end
   end
 end
