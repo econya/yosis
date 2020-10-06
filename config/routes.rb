49c1bf002cf02a76ece4dfd91b0f65c5152f3be1
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-10-06 19:34:15 +0200

    neaten routes config file

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,17 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
+  # Health check
   get '/up', to: proc {[200, {}, ['is_up']]}
 
 
-  get 'privacy',     to: 'pages#privacy'
-  get 'terms',       to: 'pages#terms'
-  get 'impressum',   to: 'pages#impressum'
-  get 'explanation', to: 'pages#explanation'
+  # Static
+  get 'privacy',         to: 'pages#privacy'
+  get 'terms',           to: 'pages#terms'
+  get 'impressum',       to: 'pages#impressum'
+  get 'explanation',     to: 'pages#explanation'
 
+  # Devise and user
   devise_for :users, controllers: {
     registrations: 'registrations',
     confirmations: 'confirmations',
@@ -23,20 +26,24 @@ Rails.application.routes.draw do
     post :stop_impersonating, on: :collection
   end
 
-  resource :contacts, only: [:new, :create]
   namespace :user do
     resource :data, only: [:show, :destroy]
   end
 
+  # Blog
   namespace :blog do
     resources :posts, only: [:index, :show]
   end
 
-
+  # Resources
+  resources :courses,  only: [:index, :show]
+  resource  :contacts, only: [:new, :create]
+  resources :place, only: :show
   resources :styles, only: [:index, :show]
 
   resource :terms_acceptance, only: [:show, :create]
 
+  # All things admin
   namespace :admin do
     resources :appointments
 
