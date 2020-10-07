00a3964e19ce8cfb01fef8ac3a8469eddb2f78ef
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-10-07 20:55:19 +0200

    add courses_general SiteSetting

--- app/controllers/admin/site_settings_controller.rb
+++ app/controllers/admin/site_settings_controller.rb
@@ -12,7 +12,7 @@ class Admin::SiteSettingsController < ApplicationController
   def index
     md_keys = [:intro, :explanation, :privacy_statement, :terms, :impressum,
                :copyright_notice, :about_us, :trial_period_cta,
-               :register_cta, :payment_details]
+               :register_cta, :courses_general, :payment_details]
     md_keys.each do |key|
       SiteSetting.find_or_create_by(key: key,
         kind: "markdown",
@@ -42,9 +42,15 @@ class Admin::SiteSettingsController < ApplicationController
     SiteSetting.find_or_create_by(key: 'your_name',
       value: t('site_settings.your_name.default'), kind: 'string')
 
-    @general_settings    = SiteSetting.where(key: ['copyright_notice', 'title', 'logo', 'favicon', 'favicon-png', 'favicon-apple-touch', 'your_name', 'payment_details'])
-    @start_page_settings = SiteSetting.where(key: ['intro', 'intro_background', 'news_line', 'register_cta', 'trial_period_cta'])
-    @pages_settings      = SiteSetting.where(key: ['about_us', 'impressum', 'privacy_statement', 'terms', 'explanation'])
+    @general_settings    = SiteSetting.where(
+      key: ['title', 'your_name', 'copyright_notice', 'payment_details',
+            'logo', 'favicon', 'favicon-png', 'favicon-apple-touch'])
+    @start_page_settings = SiteSetting.where(
+      key: ['intro', 'intro_background', 'news_line',
+            'register_cta', 'trial_period_cta'])
+    @pages_settings      = SiteSetting.where(
+      key: ['about_us', 'courses_general', 'impressum', 'privacy_statement',
+            'terms', 'explanation'])
   end
 
   def show
