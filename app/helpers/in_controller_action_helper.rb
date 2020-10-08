d4a74f245425e3cb11d7b71033c3da9398aad1d8
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-10-08 16:47:41 +0200

    remove weird syntax around helper#in_controller_action?

--- app/helpers/in_controller_action_helper.rb
+++ app/helpers/in_controller_action_helper.rb
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 module InControllerActionHelper
-  def in_controller_action?(controller: controller, action: action)
+  def in_controller_action?(controller:, action:)
     controller_name == controller && current_page?(action: action)
   end
 end
