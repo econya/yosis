bf7e5bace64998c6119f4eac8b1f4e0c0ed66b4a
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-10-08 16:41:46 +0200

    use PositionController consistently

--- app/controllers/admin/course/lesson/position_controller.rb
+++ app/controllers/admin/course/lesson/position_controller.rb
@@ -3,24 +3,7 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 class Admin::Course::Lesson::PositionController < ApplicationController
-  before_action :authenticate_user!
-  before_action :authorize_admin!
-
-  before_action :set_lesson, only: [:create, :destroy]
-
-  def create
-    @lesson.update(row_order_position: :up)
-    redirect_back fallback_location: :root_path
+  def referenced_model
+    Lesson
   end
-
-  def destroy
-    @lesson.update(row_order_position: :down)
-    redirect_back fallback_location: :root_path
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_lesson
-      @lesson = Lesson.friendly.find(params[:lesson_id])
-    end
 end
--- app/controllers/admin/course/position_controller.rb
+++ app/controllers/admin/course/position_controller.rb
@@ -2,25 +2,8 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-class Admin::Course::PositionController < ApplicationController
-  before_action :authenticate_user!
-  before_action :authorize_admin!
-
-  before_action :set_course, only: [:create, :destroy]
-
-  def create
-    @course.update(row_order_position: :up)
-    redirect_back fallback_location: :root_path
+class Admin::Course::PositionController < Admin::PositionController
+  def referenced_model
+    Course
   end
-
-  def destroy
-    @course.update(row_order_position: :down)
-    redirect_back fallback_location: :root_path
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_course
-      @course = Course.friendly.find(params[:course_id])
-    end
 end
