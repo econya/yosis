b50c589a8d3e67d69e957fd830982e4ce4512bcb
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-12-09 12:49:39 +0100

    with 10 days test, remind 8 days before

--- app/jobs/notify_ending_trial_users_job.rb
+++ app/jobs/notify_ending_trial_users_job.rb
@@ -6,7 +6,7 @@ class NotifyEndingTrialUsersJob < CronJob
   self.cron_expression= '0 7 * * *'
 
   def perform(*args)
-    User.trial_started_at(day: Date.current - 5.days).find_each do |user|
+    User.trial_started_at(day: Date.current - 8.days).find_each do |user|
       if !user.currently_subscribed?
         SubscriptionReminderMailer.trial_ends_soon(user).deliver_later
       end
