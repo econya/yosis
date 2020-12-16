be74a31e596d782bc36f0d3ef42e2d9748f5aef0
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-12-16 17:45:26 +0100

    move Subscriptionapp/models/user.rbstart_before_end validation to private section

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -14,13 +14,16 @@ class Subscription < ApplicationRecord
 
   scope :at, ->(day: DateTime.current) { where("date_start <= ? AND date_end >= ?", day, day) }
 
+  def active_today?
+    (date_start..date_end).include? DateTime.current
+  end
+
+  private
+
   def start_before_end
     if date_end < date_start
       errors.add(:date_end, :must_be_before_start_date)
     end
   end
 
-  def active_today?
-    (date_start..date_end).include? DateTime.current
-  end
 end
