6e3461637b890fb10bd457fe5ae4ac608102b7eb
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-12-16 17:23:09 +0100

    for in-payment-users show different abo box on landing page

--- test/system/payment_declaration_test.rb
+++ test/system/payment_declaration_test.rb
@@ -33,6 +33,25 @@ class PaymentDeclarationTest < ApplicationSystemTestCase
 
     visit explanation_url
     refute_selector "a[href='#{user_payment_declaration_path}']"
+  end
 
+  test "user should see 'we are working on it' message if marked paid" do
+    user = users(:user)
+    sign_in users(:user)
+
+    assert user.mark_paid_at.nil?
+    refute user.currently_subscribed?
+
+    user.subscriptions.create(date_start: DateTime.current,
+                              date_end: DateTime.current + 2.days,
+                              notes: I18n.t('user.payment_declaration.temporary_subscription'))
+    user.update(mark_paid_at: DateTime.current)
+
+    assert user.mark_paid_at
+    assert user.currently_subscribed?
+
+    visit root_path
+
+    assert_selector '#login_row', text: 'Wir sind dabei deine Zahlung zu bearbeiten'
   end
 end
