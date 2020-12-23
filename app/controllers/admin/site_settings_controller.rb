89ec37c7fc31d04ba71286068fb35c3ea65b4e83
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-12-23 11:25:29 +0100

    add asana_lexicon_header_image setting

--- app/controllers/admin/site_settings_controller.rb
+++ app/controllers/admin/site_settings_controller.rb
@@ -21,6 +21,7 @@ class Admin::SiteSettingsController < Admin::AdminController
             'register_cta', 'trial_period_cta'])
     @pages_settings      = SiteSetting.where(
       key: ['about_us_left', 'about_us_right', 'blog_background', 'courses_general', 'impressum',
+            'asana_lexicon_header_image',
             'privacy_statement', 'seminars_text', 'seminars_header_image', 'terms',
             'explanation', 'explanation_login', 'explanation_trial'])
   end
